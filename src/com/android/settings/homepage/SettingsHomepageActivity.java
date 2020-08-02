403d14d2d25f7862b4ff9580f49e7ca885d74383
AndroBuddy <anirudhchimpidi8@gmail.com>
2020-08-02 12:40:48 +0000

    Setting beatification for ui
    Signed-off-by: AndroBuddy <anirudhchimpidi8@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -67,8 +67,6 @@ public class SettingsHomepageActivity extends FragmentActivity {
         root.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
-        setHomepageContainerPaddingTop();
-
         Context context = getApplicationContext();
 
         mUserManager = context.getSystemService(UserManager.class);
@@ -131,18 +129,6 @@ public class SettingsHomepageActivity extends FragmentActivity {
             p.setMargins(l, t, r, b);
             v.requestLayout();
         }
-    }
-
-    @VisibleForTesting
-    void setHomepageContainerPaddingTop() {
-        final View view = this.findViewById(R.id.homepage_container);
-
-        final int searchBarHeight = getResources().getDimensionPixelSize(R.dimen.search_bar_height);
-        final int searchBarMargin = getResources().getDimensionPixelSize(R.dimen.search_bar_margin);
-
-        // The top padding is the height of action bar(48dp) + top/bottom margins(16dp)
-        final int paddingTop = searchBarHeight + searchBarMargin * 2;
-        view.setPadding(0 /* left */, paddingTop, 0 /* right */, 0 /* bottom */);
     }
 
     private Drawable getCircularUserIcon(Context context) {
